2e5a9257cf2bd225b4d659bd78f50432a8a948ed
limin.llm <limin.llm@alibaba-inc.com>
2015-06-27 13:53:25 +0800

    contextId加多线程上下文对接！

--- src/main/java/sendws/SendWebService.java
+++ src/main/java/sendws/SendWebService.java
@@ -2,29 +2,31 @@ package sendws;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.integration.support.channel.BeanFactoryChannelResolver;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.core.DestinationResolver;
 
 public class SendWebService {
 
-    public static void main(String[] args) {
-        final AbstractApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-integration-ws-context.xml");
-        DestinationResolver<MessageChannel> channelResolver = new BeanFactoryChannelResolver(context);
+	public static void main(String[] args) {
+		final AbstractApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-integration-ws-context.xml");
+		DestinationResolver<MessageChannel> channelResolver = new BeanFactoryChannelResolver(context);
 
-        // Compose the XML message according to the server's schema
-        String requestXml = "<echoRequest xmlns=\"http://www.cyznj.com/echo\">\n" +
-                "peter" +
-                "</echoRequest>";
+		// Compose the XML message according to the server's schema
+		String requestXml = "<getPatientByIdentityRequest xmlns=\"http://www.cyznj.com/patients\"><identity>320106700320243102</identity></getPatientByIdentityRequest>";
 
-        // Create the Message object
-        Message<String> message = MessageBuilder.withPayload(requestXml).build();
+		// Create the Message object
+		Message<String> message = MessageBuilder.withPayload(requestXml).setHeader("contextId", "pli2015").build();
+		System.out.println("request:" + message.getHeaders());
 
-        // Send the Message to the handler's input channel
-        MessageChannel channel = channelResolver.resolveDestination("echo");
-        channel.send(message);
-    }
+		// Send the Message to the handler's input channel
+		MessageChannel inputchannel = channelResolver.resolveDestination("inputchannel");
+		inputchannel.send(message);
+		context.close();
+	}
 
 }
--- src/main/java/sendws/service/EchoService.java
+++ src/main/java/sendws/service/EchoService.java
@@ -8,8 +8,9 @@ import org.springframework.messaging.MessagingException;
  * Created by limin.llm on 2014/10/26.
  */
 public class EchoService implements MessageHandler {
-    @Override
-    public void handleMessage(Message<?> message) throws MessagingException {
-        System.out.println(message.getPayload());
-    }
+	@Override
+	public void handleMessage(Message<?> message) throws MessagingException {
+		System.out.println("response:" + message.getHeaders());
+		System.out.println(message.getPayload());
+	}
 }
