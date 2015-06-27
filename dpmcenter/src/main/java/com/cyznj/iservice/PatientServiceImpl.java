c9384bd07aa4704229ad9ad99adb217257980ff6
wrh1986 <dndx61003127@gmail.com>
2015-06-27 22:27:47 +0800

    增加gateway集成

--- dpmcenter/src/main/java/com/cyznj/iservice/PatientServiceImpl.java
+++ dpmcenter/src/main/java/com/cyznj/iservice/PatientServiceImpl.java
@@ -1,5 +1,8 @@
 package com.cyznj.iservice;
 
+import com.cyznj.domain.RequestContext;
+import gateway.DataQueryGateway;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,15 +11,28 @@ import java.util.Map;
  * Created by limin.llm on 2015/6/27.
  */
 public class PatientServiceImpl implements PatientService {
+    private DataQueryGateway dataQueryGateway;
+
+    public DataQueryGateway getDataQueryGateway() {
+        return dataQueryGateway;
+    }
+
+    public void setDataQueryGateway(DataQueryGateway dataQueryGateway) {
+        this.dataQueryGateway = dataQueryGateway;
+    }
+
+
     @Override
-    public Map<String, Object> queryPatient(String uid, Map<String, Object> parameters) {
+    public Map<String, Object> queryPatient(String uid, Map<String, Object> parameters, RequestContext requestContext) {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("name", "peterli");
         return result;
     }
 
     @Override
-    public List<Map<String, Object>> queryPatientHistoryRecord(String uid, Map<String, Object> parameters) {
+    public List<Map<String, Object>> queryPatientHistoryRecord(String uid, Map<String, Object> parameters, RequestContext requestContext) {
+        String requestXml = "<getPatientByIdentityRequest xmlns=\"http://www.cyznj.com/patients\"><identity>320106700320243102</identity></getPatientByIdentityRequest>";
+        String result = dataQueryGateway.queryData(requestXml);
         return null;
     }
 }
