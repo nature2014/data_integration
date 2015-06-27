73a58d70caf475469bfcb40608fcea923c62142d
limin.llm <limin.llm@alibaba-inc.com>
2015-06-27 19:18:47 +0800

    提交测试类！

---
+++ dpmcenter/src/main/java/com/cyznj/iservice/PatientServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cyznj.iservice;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by limin.llm on 2015/6/27.
+ */
+public class PatientServiceImpl implements PatientService {
+    @Override
+    public Map<String, Object> queryPatient(String uid, Map<String, Object> context) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> queryPatientHistoryRecord(String uid, Map<String, Object> context) {
+        return null;
+    }
+}
