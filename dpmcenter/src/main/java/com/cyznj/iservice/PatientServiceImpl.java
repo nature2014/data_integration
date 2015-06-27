26db6a80b1f8061732d8bb4de3fefcb0d1616f90
wrh1986 <dndx61003127@gmail.com>
2015-06-27 22:29:43 +0800

    增加gateway集成

--- dpmcenter/src/main/java/com/cyznj/iservice/PatientServiceImpl.java
+++ dpmcenter/src/main/java/com/cyznj/iservice/PatientServiceImpl.java
@@ -3,8 +3,6 @@ package com.cyznj.iservice;
 import com.cyznj.domain.RequestContext;
 import gateway.DataQueryGateway;
 
-import com.cyznj.domain.RequestContext;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
